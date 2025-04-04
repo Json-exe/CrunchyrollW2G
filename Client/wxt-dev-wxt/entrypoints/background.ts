e64e8adaa5ce05f308f7dd0a1e94cbb49f38f364
Json-exe <96955704+Json-exe@users.noreply.github.com>
2025-04-04 22:03:28 +0200

    feat: update background service to log installation and reload Crunchyroll tabs

--- Client/wxt-dev-wxt/entrypoints/background.ts
+++ Client/wxt-dev-wxt/entrypoints/background.ts
@@ -4,19 +4,17 @@ import {registerVideoSyncService} from "@/components/services/VideoSyncService";
 registerVideoSyncService()
 
 export default defineBackground(() => {
-    console.log("Background service loaded!");
-    // chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    //     if (message.type === 'contentScriptInitialized') {
-    //         lobbyServiceInstance.isConnected = true;
-    //     } else if (message.type === 'contentScriptInvalidated') {
-    //         lobbyServiceInstance.isConnected = false;
-    //         lobbyServiceInstance.lobbyId = undefined;
-    //     } else if (message.type === 'getLobbyInfo') {
-    //         sendResponse({data: lobbyServiceInstance});
-    //         return true;
-    //     } else if (message.type === 'setLobbyId') {
-    //         lobbyServiceInstance.lobbyId = message.lobbyId;
-    //     }
-    // });
+    chrome.runtime.onInstalled.addListener(() => {
+        console.log(`Crunchyroll W2G Extension has been installed with version: ${chrome.runtime.getManifest().version}`);
+        chrome.tabs.query({url: ["*://www.crunchyroll.com*", "*://crunchyroll.com*"]}, async (tabs) => {
+            console.log(`Reloading ${tabs.length} crunchyroll tabs to allow content script to run properly.`)
+            for (const tab of tabs) {
+                if (tab.id) {
+                    await chrome.tabs.reload(tab.id)
+                }
+            }
+        })
+    })
+    console.log("Background service loaded successfully!");
 });
 
